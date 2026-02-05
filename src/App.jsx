fa666bc0f2cce1809fd098eeb196202082679f3b
gabrielsantos190509 <santosg85195@gmail.com>
2026-02-05 08:58:28 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 
-export default function App() {
+function App() {
   return (
     <>
       <div className="auth-container">
         <div className="auth-background">
           <div className="gradient-orb orb-1"></div>
           <div className="gradient-orb orb-2"></div>
-          <div className="gradient-orb orb-2"></div>
+          <div className="gradient-orb orb-3"></div>
         </div>
       </div>
     </>
   )
 }
 
+export default App
